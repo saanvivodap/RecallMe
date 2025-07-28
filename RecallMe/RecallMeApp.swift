f5f43dc39f73a80867f4fad0ba075b5071efaf45
Saanvi <saanvivodapally@gmail.com>
2025-07-28 14:11:14 -0700

    Initial Commit

---
+++ RecallMe/RecallMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallMeApp.swift
+//  RecallMe
+//
+//  Created by Saanvi on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct RecallMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
