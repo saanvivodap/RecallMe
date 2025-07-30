58dcbe3ac5b751d6091f9194ea871ac188f9bfd3
Akshara <sweetakshara.0715@gmail.com>
2025-07-30 14:05:05 -0700

    Correct View Change

--- RecallMe/RecallMeApp.swift
+++ RecallMe/RecallMeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RecallMeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContactListView()
                 .modelContainer(for: ContactItem.self)
         }
     }
