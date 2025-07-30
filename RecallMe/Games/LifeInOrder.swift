ed6d9a9289c7f5868fe9c5fe0aad39dc819697af
Saanvi <saanvivodapally@gmail.com>
2025-07-30 14:25:09 -0700

    Created files for the games & organized files

---
+++ RecallMe/Games/LifeInOrder.swift
@@ -0,0 +1,18 @@
+//
+//  LifeInOrder.swift
+//  RecallMe
+//
+//  Created by Saanvi on 7/30/25.
+//
+
+import SwiftUI
+
+struct LifeInOrder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LifeInOrder()
+}
---
+++ RecallMe/ToDoList.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoList.swift
+//  RecallMe
+//
+//  Created by Saanvi on 7/30/25.
+//
+
+import SwiftUI
+
+struct ToDoList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoList()
+}
