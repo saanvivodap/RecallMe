c654a446d04c993d8d12e9114773115a0be7e45d
Mahado <mabdirahman242424@gmail.com>
2025-07-31 11:39:15 -0700

    Finished ToDoList

--- ToDoItem.swift
+++ ToDoItem.swift
@@ -6,18 +6,3 @@
 //
 
 import Foundation
-import SwiftData
-
-class toDoItem{
-    var title: String
-    var isImportant: Bool
-    
-    init(title: String, isImportant: Bool) {
-        self.title = title
-        self.isImportant = isImportant
-    }
-}
-
-#Preview {
-    ToDoItem()
-}
