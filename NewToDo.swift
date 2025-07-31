137b9957fb3592f181a6d58a6dd11bdb67bec1ad
Mahado <mabdirahman242424@gmail.com>
2025-07-31 10:17:49 -0700

    Made a Main View

--- NewToDo.swift
+++ NewToDo.swift
@@ -11,38 +11,35 @@ struct NewToDo: View {
     @Bindable var toDoItem: ToDoItem
     @Environment(\.modelContext) var modelContext
     @Binding var showNewTask: Bool
-    var body: some ToDoItem {
-        var body: some View {
-            VStack{
-                Text("Task Title:")
-                    .font(.title)
-                    .fontWeight(.bold)
-                TextField("Enter a Text description", text: $toDoItem.title, axis: .vertical)
-                    .padding()
-                    .background(Color(.systemGroupedBackground))
-                    .cornerRadius(15)
-                    .padding()
-                Toggle(isOn: $toDoItem.isImportant) {
-                    Text("Is it important")
-                }
-                Button {
-                    addToDo()
-                    showNewTask = false
-                } label: {
-                    Text("Save")
-                }
+    var body: some View {
+        VStack{
+            Text("Task Title:")
+                .font(.title)
+                .fontWeight(.bold)
+            TextField("Enter a Text description", text: $toDoItem.title, axis: .vertical)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding()
+            Toggle(isOn: $toDoItem.isImportant) {
+                Text("Is it important")
             }
-            .padding()
+            Button {
+                addToDo()
+                showNewTask = false
+            }label: {
+                Text("Save")
+            }
         }
-        func addToDo() {
-            let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
-            modelContext.insert(toDo)
+        .padding()
         }
+    func addToDo() {
+        let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+        modelContext.insert(toDo)
     }
+}
     
     #Preview {
         NewToDo(toDoItem: ToDoItem(title: "", isImportant: false), showNewTask: .constant(false))
-        
     }
-    
-}
+
